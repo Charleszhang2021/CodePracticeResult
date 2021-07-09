d4472d28397d548f5ec7bc2ec25cb94c91e6fca9
Charleszhang2021 <86215181+Charleszhang2021@users.noreply.github.com>
2021-07-09 15:28:08 -0400

    Create 右手定则.swift

---
+++ Charles/学习编程一/右手定则.swift
@@ -0,0 +1,23 @@
+func navigateAroundWall() {
+    if isBlockedRight {
+        moveForward()
+        
+    } else  {
+        turnRight()
+        moveForward()
+        
+    }
+    if isOnGem {
+        collectGem()
+        turnLeft()
+        turnLeft()
+        
+    }
+    
+    
+}
+while !isOnClosedSwitch {
+    navigateAroundWall()
+    
+}
+toggleSwitch()
